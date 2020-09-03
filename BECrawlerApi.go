7a5333e826a5107b79ee4104275b5a211062673b
NgoanN <wapa2pro@gmail.com>
2020-09-03 23:28:52 +0700

    Dont download file

--- BECrawlerApi.go
+++ BECrawlerApi.go
@@ -4,16 +4,11 @@ import (
   "encoding/json"
   "github.com/gocolly/colly"
   "github.com/gocolly/colly/proxy"
-  "html/template"
-  "io"
   "log"
   "net/http"
   "net/url"
   "os"
   "regexp"
-  "archive/zip"
-  "path/filepath"
-  "strings"
   "math/rand"
   "github.com/gorilla/mux"
   "github.com/rs/cors"
@@ -48,11 +43,7 @@ func main() {
 
   // Handle API routes
   api := r.PathPrefix("/").Subrouter()
-  api.HandleFunc("/", HomePage)
   api.HandleFunc("/download", DownloadHandler)
-
-  // Serve static files
-  r.PathPrefix("/assets/").Handler(http.StripPrefix("/assets/", http.FileServer(http.Dir("assets/"))))
 
   handlerCORS := cors.Default().Handler(r)
 
@@ -64,19 +55,6 @@ func main() {
   }
   log.Println("http://0.0.0.0:" + port)
   log.Fatal(srv.ListenAndServe())
-}
-
-func HomePage(w http.ResponseWriter, r *http.Request){
-    HomePageVars := PageVariables{}
-
-    t, err := template.ParseFiles("homepage.html") //parse the html file homepage.html
-    if err != nil { // if there is an error
-  	  log.Print("template parsing error: ", err) // log it
-  	}
-    err = t.Execute(w, HomePageVars) //execute the template and pass it the HomePageVars struct to fill in the gaps
-    if err != nil { // if there is an error
-  	  log.Print("template executing error: ", err) //log it
-  	}
 }
 
 func DownloadHandler(w http.ResponseWriter, r *http.Request){
@@ -101,18 +79,17 @@ func DownloadHandler(w http.ResponseWriter, r *http.Request){
 }
 
 func Crawl(productId string) (string, bool) {
-  GetProxies()
-  preSavePath := "assets/"
-  productName := preSavePath + productId + ".png"
-  os.MkdirAll(preSavePath, os.ModePerm)
+  GetProxyFromFile()
 
-  if CrawlWithoutProxy(productId, preSavePath, productName) {
-    return productName, true
+  crawlStatus, fileUrl := CrawlWithoutProxy(productId)
+
+  if crawlStatus {
+    return fileUrl, crawlStatus
   }
 
   c := colly.NewCollector()
 
-  var downloaded bool;
+  var getSuccess bool;
   for i := 0; i < 10; i++ {
     randomIndex := rand.Intn(len(proxyServers))
     pickProxyServer := proxyServers[randomIndex]
@@ -133,12 +110,11 @@ func Crawl(productId string) (string, bool) {
 
       if len(designRegex.FindStringSubmatch(decodedValue)) > 0 {
         filePath := designRegex.FindStringSubmatch(decodedValue)[1]
-        fileUrl := "https://m.media-amazon.com/images/I/" + filePath
-        DownloadFile(productName, fileUrl)
-        log.Println("Downloaded:", productName)
-        downloaded = true
+        fileUrl = "https://m.media-amazon.com/images/I/" + filePath
+        log.Println(productId, ":", fileUrl, " -> OK")
+        getSuccess = true
       } else {
-        log.Println("Failed:", productName)
+        log.Println(productId, ":", " -> Failed")
       }
     })
 
@@ -147,15 +123,16 @@ func Crawl(productId string) (string, bool) {
     })
 
     c.Visit("https://www.amazon.com/dp/" + productId)
-    if downloaded {
+    if getSuccess {
       break
     }
   }
-  return productName, downloaded
+  return fileUrl, getSuccess
 }
 
-func CrawlWithoutProxy(productId string, preSavePath string, productName string) (bool) {
+func CrawlWithoutProxy(productId string) (bool, string) {
   var status bool
+  var fileUrl string
   c := colly.NewCollector()
 
   c.OnHTML("#imgTagWrapperId img", func(e *colly.HTMLElement) {
@@ -164,12 +141,11 @@ func CrawlWithoutProxy(productId string, preSavePath string, productName string)
 
     if len(designRegex.FindStringSubmatch(decodedValue)) > 0 {
       filePath := designRegex.FindStringSubmatch(decodedValue)[1]
-      fileUrl := "https://m.media-amazon.com/images/I/" + filePath
-      DownloadFile(productName, fileUrl)
-      log.Println("Downloaded:", productName)
+      fileUrl = "https://m.media-amazon.com/images/I/" + filePath
+      log.Println(productId, ":", fileUrl, " -> OK")
       status = true
     } else {
-      log.Println("Failed:", productName)
+      log.Println(productId, ":", " -> Failed")
     }
   })
 
@@ -179,93 +155,10 @@ func CrawlWithoutProxy(productId string, preSavePath string, productName string)
 
   c.Visit("https://www.amazon.com/dp/" + productId)
 
-  return status
+  return status, fileUrl
 }
 
-func DownloadFile(filepath string, url string) error {
-	// Get the data
-	resp, err := http.Get(url)
-	if err != nil {
-		return err
-	}
-	defer resp.Body.Close()
-
-	// Create the file
-	out, err := os.Create(filepath)
-	if err != nil {
-		return err
-	}
-	defer out.Close()
-
-	// Write the body to file
-	_, err = io.Copy(out, resp.Body)
-	return err
-}
-
-// ZIP FILE
-func zipit(source, target string) error {
-	zipfile, err := os.Create(target)
-	if err != nil {
-		return err
-	}
-	defer zipfile.Close()
-
-	archive := zip.NewWriter(zipfile)
-	defer archive.Close()
-
-	info, err := os.Stat(source)
-	if err != nil {
-		return nil
-	}
-
-	var baseDir string
-	if info.IsDir() {
-		baseDir = filepath.Base(source)
-	}
-
-	filepath.Walk(source, func(path string, info os.FileInfo, err error) error {
-		if err != nil {
-			return err
-		}
-
-		header, err := zip.FileInfoHeader(info)
-		if err != nil {
-			return err
-		}
-
-		if baseDir != "" {
-			header.Name = filepath.Join(baseDir, strings.TrimPrefix(path, source))
-		}
-
-		if info.IsDir() {
-			header.Name += "/"
-		} else {
-			header.Method = zip.Deflate
-		}
-
-		writer, err := archive.CreateHeader(header)
-		if err != nil {
-			return err
-		}
-
-		if info.IsDir() {
-			return nil
-		}
-
-		file, err := os.Open(path)
-		if err != nil {
-			return err
-		}
-		defer file.Close()
-		_, err = io.Copy(writer, file)
-		return err
-	})
-
-  log.Println("Ziped:", target)
-	return err
-}
-
-func GetProxies() {
+func GetProxyFromFile() {
 	proxyServers = []string{}
 
 	file, err := os.Open("ready_proxy_servers.txt")
